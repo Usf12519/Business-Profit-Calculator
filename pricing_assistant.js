ad6fe42e1c7960cfabaf0731d612354af0b7e79a
Cameron Smith <Cs11@usf.edu>
2026-01-28 14:25:45 -0500

    first commit

---
+++ pricing_assistant.js
@@ -0,0 +1,23 @@
+let productName = ("Coffee Mug");
+let costPerUnit = ("13.75");
+let basePrice = ("15.55");
+let discountRate = ("0.35");
+let salesTaxRate = ("0.05");
+let fixedMonthlyCosts = ("rent = $750");
+
+// Change
+discountedPrice = basePrice * (1 - discountRate);
+finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
+profitPerUnit = finalPriceWithTax - costPerUnit;
+breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+isProfitablePerUnit = (profitPerUnit > 0);
+
+
+console.log(productName);
+console.log(discountedPrice);
+console.log(finalPriceWithTax);
+console.log(profitPerUnit);
+console.log(breakEvenUnits);
+console.log(isProfitablePerUnit);
+
+
